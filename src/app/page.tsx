e900e7bdc1667f038884cb444ab12177d0ae8edd
Samit Koyom <samit90daytalk@hotmail.com>
2025-10-15 23:49:26 +0700

    update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h1>Hello Next.js!</h1>
+      <h1>Hello Next.js! 555+</h1>
       
       <p className="text-center max-w-lg">
         This is a minimal Next.js app deployed with Docker. You can start editing the code in <code>src/app/page.tsx</code>.
